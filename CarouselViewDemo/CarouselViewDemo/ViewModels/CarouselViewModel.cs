9e780fdfa76aa1bfea26c5b10941805b42febeab
chetan rawat <chetan.rawat@hicom.in>
2019-12-18 11:05:02 +0530

    Initial Commit

---
+++ CarouselViewDemo/CarouselViewDemo/ViewModels/CarouselViewModel.cs
@@ -0,0 +1,73 @@
+﻿using CarouselViewDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CarouselViewDemo.ViewModels
+{
+    public class CarouselViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<CarouselModel> _Items;
+        ObservableCollection<CarouselModel> _Items1;
+        public ObservableCollection<CarouselModel> Items
+        {
+            get
+            {
+                return _Items;
+            }
+            set
+            {
+                _Items = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<CarouselModel> Items1
+        {
+            get
+            {
+                return _Items1;
+            }
+            set
+            {
+                _Items1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public CarouselViewModel()
+        {
+            Items = new ObservableCollection<CarouselModel>();
+            Items1 = new ObservableCollection<CarouselModel>();
+            AddItems();
+            AddItems1();
+        }
+
+        void AddItems()
+        {
+            _Items.Add(new CarouselModel { Images = "Bg1.jpg", BgColors = Color.White });
+            _Items.Add(new CarouselModel { Images = "Bg2.png", BgColors = Color.White});
+            _Items.Add(new CarouselModel { Images = "Bg3.jpg", BgColors = Color.White });
+            _Items.Add(new CarouselModel { Images = "spiderman.png", BgColors = Color.Blue });
+            _Items.Add(new CarouselModel { Images = "Bg2.png", BgColors = Color.White });
+        }
+
+        void AddItems1()
+        {
+            _Items1.Add(new CarouselModel { Images = "Bg4.jpg", BgColors = Color.White });
+            _Items1.Add(new CarouselModel { Images = "Bg5.jpg", BgColors = Color.White });
+            _Items1.Add(new CarouselModel { Images = "Bg6.jpg", BgColors = Color.White });
+            _Items1.Add(new CarouselModel { Images = "Bg7.jpg", BgColors = Color.White });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CarouselViewDemo/CarouselViewDemo/Views/CarouselViewPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CarouselViewDemo.ViewModels;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CarouselViewPage : ContentPage
+	{
+        CarouselViewModel a = null;
+        public CarouselViewPage ()
+		{
+			InitializeComponent ();
+            int slidePosition = 0;
+             a =  new ViewModels.CarouselViewModel();
+            this.BindingContext = a;
+           
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                if (a.Items.Count == 0 || a.Items == null)
+                {
+                    return true;
+                }
+                slidePosition++;
+                if (slidePosition == a.Items.Count) slidePosition = 0;
+                carouselView.Position = slidePosition;
+                return true;
+            });
+        }
+    }
+}
